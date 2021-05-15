a290b440574d0d80d939273b824a2e7cea207cb7
CastroOlympias <blackcarrera@msn.com>
2021-05-14 21:38:08 -0600

    player health less than zero, is now told to be zero to avoid negative values or a zombie player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -5,7 +5,7 @@ jest.mock('../lib/Potion');
 //console.log(new Potion());
 
 const Player = require('../lib/Player');
-const { expect, it } = require('@jest/globals');
+const { expect, it, test } = require('@jest/globals');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -38,3 +38,30 @@ it('gets inventory from player or returns false', () => {
 
     // expect(player.getInventory()).toEqual(false);
 })
+
+test("get player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if the player is avlive or not', () => {
+    const player = new Player('Dave');
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+})
+
+it("subtracts from the player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
