4826e9ce899cf452817333ad51d97860b53fecee
CastroOlympias <blackcarrera@msn.com>
2021-05-14 21:01:50 -0600

    turned 'this' method into prototypes

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,10 +2,10 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
+//console.log(new Potion());
 
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
+const { expect, it } = require('@jest/globals');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -19,3 +19,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+it("gets Player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+})
+
+it('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    // player.Inventroy = [];
+
+    // expect(player.getInventory()).toEqual(false);
+})
